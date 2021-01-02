4d2d95adbf98952b972c1d8ed50f3830c0870632
przemek <przemek@pmtsoftware.eu>
2021-01-02 23:32:15 +0100

    servant web api initial project

--- web-server/Main.hs
+++ web-server/Main.hs
@@ -1,10 +1,39 @@
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE DataKinds #-}
+
 module Main where
 
 import qualified MyLib (someFunc)
+import Servant
+import GHC.Generics
+import Data.Aeson
+import Network.Wai.Handler.Warp (run)
+
+
+-- GET      /invoices 
+-- GET      /invoices/:id 
+-- PUT      /invoices/:id
+-- DELETE   /invoices/:id
+type InvoiceAPI = 
+    "invoices" :> Get '[JSON] [Invoice]
+
+data Invoice = Invoice 
+    { id :: String 
+    , no :: String 
+    } deriving (Show, Generic)
+
+instance ToJSON Invoice
+
+allInvoices = [ Invoice "1" "FV 1", Invoice "2" "FV 2"]
+
+server :: Server InvoiceAPI 
+server = return allInvoices
+
+invoicesAPI :: Proxy InvoiceAPI
+invoicesAPI = Proxy
 
 main :: IO ()
-main = do
-  putStrLn "Hello, Haskell!"
-  MyLib.someFunc
-
-test = undefined
+main = run 8081 app 
+    where 
+        app = serve invoicesAPI server
